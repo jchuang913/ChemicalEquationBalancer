56747f6fbf2bd6066af236194de75d857e5557bc
Justin Huang <justinh7@illinois.edu>
2018-12-02 18:23:14 -0600

    Initial commit

---
+++ app/src/main/java/com/example/justin/chemicalequationbalancer/ChemicalEquationBalancer.java
@@ -0,0 +1,13 @@
+package com.example.justin.chemicalequationbalancer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChemicalEquationBalancer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chemical_equation_balancer);
+    }
+}
